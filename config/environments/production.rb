273f62c6b23f37ee6ea5e59fb916328f0a9fb4b1
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-03-30 20:22:06 +0300

    Mail setup in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Todo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'http://todo-yourdomainhere.rhcloud.com' }
+  config.action_mailer.asset_host = 'http://todo-yourdomainhere.rhcloud.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'slawache@gmail.com',
+    password: 'fucking29shit',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
