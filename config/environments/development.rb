0f6dba1262781f4b2003f3843e41dbac2c2c0cdf
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-03-31 10:34:02 +0300

    Change development.rb and database.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,23 +27,8 @@ Todo::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  #config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
-
-  config.action_mailer.default_url_options = { :host => 'http://mysterious-basin-9211.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "http://mysterious-basin-9211.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
 end
