2ddb4451c90a413b72a89d8036aac918b9328f39
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-04-01 18:16:13 +0300

    Change settings for mail in production.db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,18 +78,14 @@ Todo::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-
-config.action_mailer.default_url_options = { :host => 'http://todo-todorubyonrails.rhcloud.com' }
-config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i -t'
+# }
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address: "smtp.mailgun.com",
-  port: 587,
-  domain: "sandbox3062952b83f74654878c37ac8841fbdd.mailgun.org",
-  authentication: "plain",
-  user_name: "postmaster@sandbox3062952b83f74654878c37ac8841fbdd.mailgun.org",
-  password: "2acac8e75b878527f40bec6cf73a7c74"
-}
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'no-reply@todorailsapp.com'}
+
 end
